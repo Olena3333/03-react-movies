854b5e7bc7ce734c5ac06c7cad509482fd6583c0
Olena Solonikova <137723764+Olena3333@users.noreply.github.com>
2026-07-26 22:35:14 +0200

    vite

---
+++ src/components/app/App.tsx
@@ -0,0 +1,14 @@
+import SearchBar from "../searchBar/SearchBar";
+import MovieGrid from "../movieGrid/MovieGrid";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <SearchBar />
+      <MovieGrid />
+    </>
+  );
+}
+
+export default App;
