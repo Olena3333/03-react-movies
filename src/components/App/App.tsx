90d097150e229c81511df6b7a48f7593db696206
Olena Solonikova <137723764+Olena3333@users.noreply.github.com>
2026-08-04 00:27:07 +0200

    fix1

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 
-import SearchBar from "./SearchBar/SearchBar";
-import MovieGrid from "./MovieGrid/MovieGrid";
-import Loader from "./Loader/Loader";
-import ErrorMessage from "./ErrorMessage/ErrorMessage";
-import MovieModal from "./MovieModal/MovieModal";
+import SearchBar from "../SearchBar/SearchBar";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import MovieModal from "../MovieModal/MovieModal";
 
 import { fetchMovies } from "../../services/movieService";
 import type { Movie } from "../../types/movie";
